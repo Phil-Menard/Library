15d68b4a188f9c8ebe03e61af469f57ad1e48b83
Monito <phil.menard1668@gmail.com>
2022-12-22 15:24:40 -0300

    Display the details of the book
    Display the author, title, pages and year correctly, but still need style.
    Miss the remove button/function and the read button/function.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const main = document.querySelector('.main');
 const submitButton = document.querySelector('button');
 const titleValue = document.querySelector('#title');
 const authorValue = document.querySelector('#author');
@@ -7,7 +8,6 @@ const yearValue = document.querySelector('#year');
 //VARIABLES
 const values = [titleValue, authorValue, pagesValue, yearValue];
 const books = [];
-
 //EVENT LISTENER
 submitButton.addEventListener('click', getValues);
 
@@ -21,10 +21,26 @@ function Book(title, author, pages, year) {
 
 //FUNCTIONS
 function getValues(e)    {
-    books.push(new Book(titleValue.value, authorValue.value, pagesValue.value, yearValue.value));
-    for (let i = 0; i < values.length; i++) {
-        values[i].value = "";
-    }
     e.preventDefault(); //avoid the datas to be send to a non-existant server
-    //displayBook(); A CREER
+    books.push(new Book(titleValue.value, authorValue.value, pagesValue.value, yearValue.value));
+    displayBook(titleValue.value, authorValue.value, pagesValue.value, yearValue.value);
+    for (let i = 0; i < values.length; i++) {
+        values[i].value = "";   //reinitiate the values on the form
+    }
+    //add book in main with length-1?
+}
+
+function displayBook(title, author, pages, year)   {
+    let card = document.createElement('div');
+    card.className = "card";
+    let titleDiv = document.createElement('div');
+    let authorDiv = document.createElement('div');
+    let pagesDiv = document.createElement('div');
+    let yearDiv = document.createElement('div');
+    titleDiv.textContent = "Title : " + title;
+    authorDiv.textContent = "Author : " + author;
+    pagesDiv.textContent = "Pages : " + pages;
+    yearDiv.textContent = "Year : " + year;
+    card.append(titleDiv, authorDiv, pagesDiv, yearDiv);
+    main.appendChild(card);
 }
